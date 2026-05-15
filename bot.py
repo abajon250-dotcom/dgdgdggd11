5512cc747c357aaf13885d0906f1138621e4be9e
abajon250-dotcom <abajon250@gmail.com>
2026-05-15 21:03:52 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from typing import List, Dict, Any, Optional, Tuple
 
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
+from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
